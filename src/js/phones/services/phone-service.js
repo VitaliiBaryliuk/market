ab3ae0f5ec8d5cdbfb0cd380c2d95b8138a665bc
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-03-19 23:15:18 +0200

    fixed problems with table phones.json

--- src/js/phones/services/phone-service.js
+++ src/js/phones/services/phone-service.js
@@ -1,23 +1,23 @@
 const PhoneService = {
-  getAll(callback) {
-    let xhr = new XMLHttpRequest();
+  getAll: (callback) => {
+    const xhr = new XMLHttpRequest();
     xhr.open(
-      'GET', 
-      `https://mate-academy.github.io/phone-catalogue-static/phones/phones.json`,
-      true
+      'GET',
+      'https://mate-academy.github.io/phone-catalogue-static/api/phones.json',
+      true,
     );
     xhr.send();
-    
+
     xhr.onload = () => {
       if (xhr.status !== 200) {
         return [];
       }
 
-      let phones = JSON.parse(xhr.responseText); 
+      const phones = JSON.parse(xhr.responseText);
       callback(phones);
-    }
+    };
   },
-
+  
   getById(phoneId, callback) {
     let xhr = new XMLHttpRequest(); 
     xhr.open('GET', `js/phones-base/${phoneId}.json`, true);
